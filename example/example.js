0aa0f95a4ceeec8d332aeafc733db792f4cc143b
gitztech <gjpyzer+github@gmail.com>
2017-09-13 11:44:03 +0100

    Simplify usage of resemble

--- example/example.js
+++ example/example.js
@@ -1,28 +1,30 @@
-const fs = require('mz/fs');
 const puppeteer = require('puppeteer');
 const Resemble = require('../src/resemble');
+const {assert} = require('chai');
 let browser, page, resemble;
+const headingSelector = '.container .hero-text h2';
+const containerSelector = '.container';
 
 function testName() {
     return this.test.fullTitle().replace(/\s/g, '_').toLowerCase();
 }
 
-describe('My visual test wooo', async function() {
+describe('My ', async function() {
     before(async() => {
         browser = await puppeteer.launch();
         page = await browser.newPage();
         page.setViewport({ width: 1000, height: 1000, deviceScaleFactor: 1 });
-
-        await page.goto('http://huddle.github.io/Resemble.js/');
-        await page.click('#example-images');
-        await page.waitForSelector('#dropzone2 img');
-
+        await page.goto('https://www.huddle.com');
         resemble = new Resemble({page: page, path: '.'});
-
     });
 
-    it('Look at image', async function() {
-        await resemble.visualCompare('#dropzone2 img', testName.call(this));
+    it ('Check header text', async function() {
+        await assert.waitForSelector(headingSelector);
+        assert.equal(headingSelector, 'Secure document collaboration for government and enterprise.');
+    });
+
+    it('Look heading', async function() {
+        await resemble.visualCompare(containerSelector, testName.call(this));
     });
 
     after(async () => {

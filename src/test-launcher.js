359c57f51bd37ea060e55c2d93b97eae4a0eb19c
gitztech <gjpyzer+github@gmail.com>
2018-02-03 16:12:41 +0000

    Cleanup launcher

--- src/test-launcher.js
+++ src/test-launcher.js
@@ -4,74 +4,67 @@ const mochateerInterface = require('../lib/test-interface');
 const {browserInstance} = require('../lib/test-controller');
 const recursiveReadSync = require('recursive-readdir-sync');
 
-module.exports = class Launcher {
-    constructor({
-                    testDir,
-                    testFilter,
-                    shouldRebaseVisuals,
-                    reportDir,
-                    componentTestUrlFactory,
-                    componentTestVisualPathFactory,
-                    visualThreshold,
-                    afterHook
-                }) {
-        componentTestUrlFactory = componentTestUrlFactory || (component => {
-            return component.url;
-        });
+module.exports = function Launcher({
+    testDir,
+    testFilter,
+    shouldRebaseVisuals,
+    reportDir,
+    componentTestUrlFactory,
+    componentTestVisualPathFactory,
+    visualThreshold,
+    afterHook
+}) {
+    componentTestUrlFactory = componentTestUrlFactory || (component => component.url);
+    componentTestVisualPathFactory = componentTestVisualPathFactory || (component => path.join(testDir, `/screenshots/${component.name}`));
 
-        componentTestVisualPathFactory = componentTestVisualPathFactory || (component => {
-            return path.join(testDir, `/screenshots/${component.name}`);
-        });
+    Mocha.interfaces['mochateer'] = mochateerInterface(componentTestUrlFactory, componentTestVisualPathFactory, visualThreshold, shouldRebaseVisuals);
 
-        Mocha.interfaces['mochateer'] = mochateerInterface(componentTestUrlFactory, componentTestVisualPathFactory, visualThreshold, shouldRebaseVisuals);
-
-        const mocha = new Mocha({
-            timeout: 10000, ui: 'mochateer', reporter: 'mocha-multi-reporters',  reporterOptions: {
-                'reporterEnabled': 'mocha-junit-reporter, mochawesome',
-                'mochaJunitReporterReporterOptions': {
-                    'mochaFile': reportDir + '/junit-custom.xml'
-                }, 'mochawesomeReporterOptions': {
-                    'reportDir': reportDir,
-                    'reportFilename': 'test-report',
-                    'reportTitle': 'Test Report',
-                    'reportPageTitle': 'Test Report'
-                }
-            }
-        });
-
-        let files = null;
-
-        try {
-            files = recursiveReadSync(testDir);
-        } catch (err) {
-            if (err.errno === 34) {
-                throw Error('Path does not exist');
-            } else {
-                throw err;
+    const mocha = new Mocha({
+        timeout: 10000, ui: 'mochateer', reporter: 'mocha-multi-reporters', reporterOptions: {
+            'reporterEnabled': 'mocha-junit-reporter, mochawesome',
+            'mochaJunitReporterReporterOptions': {
+                'mochaFile': reportDir + '/junit-custom.xml'
+            }, 'mochawesomeReporterOptions': {
+                'reportDir': reportDir,
+                'reportFilename': 'test-report',
+                'reportTitle': 'Test Report',
+                'reportPageTitle': 'Test Report'
             }
         }
+    });
 
-        if (testFilter) {
-            files.filter(file => {
-                return file.indexOf(testFilter) !== -1 && path.basename(file).substr(-7) === 'test.js';
-            }).forEach(file => {
-                mocha.addFile(file);
-            });
+    let files = null;
+
+    try {
+        files = recursiveReadSync(testDir);
+    } catch (err) {
+        if (err.errno === 34) {
+            throw Error('Path does not exist');
         } else {
-            files.forEach(file => {
-                mocha.addFile(file);
-            });
+            throw err;
         }
-
-        (async function() {
-            await browserInstance.launch();
-
-            mocha.run(async () => {
-                await browserInstance.close();
-                afterHook && afterHook();
-            });
-        })();
     }
+
+    if (testFilter) {
+        files.filter(file => {
+            return file.indexOf(testFilter) !== -1 && path.basename(file).substr(-7) === 'test.js';
+        }).forEach(file => {
+            mocha.addFile(file);
+        });
+    } else {
+        files.forEach(file => {
+            mocha.addFile(file);
+        });
+    }
+
+    (async() => {
+        await browserInstance.launch();
+
+        mocha.run(async() => {
+            await browserInstance.close();
+            afterHook && afterHook();
+        });
+    })();
 };
 
 

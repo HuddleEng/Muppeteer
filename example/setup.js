6570749d3d91d60aa2f2db6f69346e9293d6effd
gitztech <gjpyzer+github@gmail.com>
2018-02-22 01:29:53 +0000

    #15 Check for required dependencies

--- example/setup.js
+++ example/setup.js
@@ -1,16 +1,19 @@
 const {promisify} = require('util');
 const extract = promisify(require('extract-zip'));
 const path = require('path');
-
+const {CONSOLE_PREFIX} = require('../src/console-helpers');
+const {checkDependencies} = require('../src/check-dependencies');
 const file = 'todomvc-react.zip';
 const source = path.join(__dirname, file);
 
 (async() => {
+    console.log(`${CONSOLE_PREFIX} Running post-setup script...`.green);
     const extractErrors = await extract(source, { dir: __dirname });
     
     if (!extractErrors) {
-        console.log('Post-setup script complete');
+        checkDependencies();
+        console.log(`${CONSOLE_PREFIX} Post-setup script complete`.green);
     } else {
-        throw Error(`Unable to extract ${file}`);
+        throw Error(`${CONSOLE_PREFIX} Unable to extract ${file}`.red);
     }
 })();
---
+++ src/check-dependencies.js
@@ -0,0 +1,49 @@
+/**
+ *
+ * This file exports a function to check for required or recommended dependencies. Docker is no a hard dependency, but
+ * it is recommended for deterministic visual test results across different platforms.
+ *
+ **/
+
+const shell = require('shelljs');
+const colors = require('colors');
+const {CONSOLE_PREFIX} = require('./console-helpers');
+
+const dependencyList = [{
+    name: 'docker',
+    displayName: 'Docker',
+    required: false,
+    instructions: 'You need Docker to run tests in a container. You can download it from https://www.docker.com/',
+}];
+
+const checkDependency = (dependency, isRequired = dependency.required) => {
+    if (typeof dependency === 'string') {
+        const found = dependencyList.find(d => d.name.toLowerCase() === dependency.toLowerCase());
+        if (found) {
+            dependency = found;
+        } else {
+            console.error(`${CONSOLE_PREFIX} No dependency matches the name ${dependency}`.red);
+            return false;
+        }
+    }
+
+    if (!!shell.which(dependency.name)) {
+        console.log(`${CONSOLE_PREFIX} ${dependency.displayName} dependency was found`.green);
+        return true;
+    } else {
+        console.log(`${CONSOLE_PREFIX} ${dependency.displayName} is missing. ${dependency.instructions}`[isRequired ? 'red' : 'yellow']);
+    }
+
+    return false;
+};
+
+const checkDependencies = () => {
+    dependencyList.forEach(dependency => {
+        return checkDependency(dependency);
+    });
+};
+
+module.exports = {
+    checkDependency,
+    checkDependencies
+};

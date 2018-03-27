d6b0aaf8d6350826058e24ff4dcd937c0ec952be
gidztech <gjpyzer+github@gmail.com>
2018-03-27 14:33:02 +0100

    #29 Make script work on Windows, remove jq requirement

--- update-docker-chrome.sh
+++ update-docker-chrome.sh
@@ -2,11 +2,14 @@
 set -e
 
 if [ -z "$1" ]; then
-    # get latest tag for repo (auth token not needed for this API)
-    LATEST_TAG=$(curl -s https://hub.docker.com/v2/repositories/alpeware/chrome-headless-stable/tags/ | jq -r ".results|.[1].name")
+    # Get latest tag for repo (auth token not needed for this API)
+    # jq would be nicer here, but it's an extra dependency (jq -r ".results|.[1].name")
+    LATEST_TAG=$(curl -s https://hub.docker.com/v2/repositories/alpeware/chrome-headless-stable/tags/ | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["results"][1]["name"]')
 else 
     LATEST_TAG="ver-$1"
 fi
 
-sed -i "" "s/:.*/:$LATEST_TAG/g" chrome/Dockerfile
+# Replace the tag in the Dockerfile
+# The bak file is used to deal with `sed` differences across OSs
+sed -i.bak "s/:.*/:$LATEST_TAG/g" chrome/Dockerfile && rm chrome/Dockerfile.bak
 echo "Chrome version set to $LATEST_TAG"

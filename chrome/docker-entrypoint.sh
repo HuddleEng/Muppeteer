f33ff18d6c5b25a37f55b293dd55f9c46653537d
gidztech <gjpyzer+github@gmail.com>
2018-07-24 22:58:56 +0100

    fdsfsd

--- chrome/docker-entrypoint.sh
+++ chrome/docker-entrypoint.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-
-HOST_DOMAIN="host.docker.internal"
-ping -q -c1 $HOST_DOMAIN > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-  HOST_IP=$(ip route | awk 'NR==1 {print $3}')
-  echo -e "$HOST_IP\t$HOST_DOMAIN" >> /etc/hosts
-fi
-
-# Here is the original entry point.
-curl -sS host.docker.internal:3000
-cat /etc/hosts
+echo -e "`/sbin/ip route|awk '/default/ { print $3 }'`\tdocker.host.internal" | sudo tee -a /etc/hosts > /dev/null
--- e2e-tests/network.js
+++ e2e-tests/network.js
@@ -1,12 +1,13 @@
 let HOST;
 
-if (process.platform === 'win32') {
-    HOST = 'docker.for.win.host.internal'; // Windows
-} else if (process.platform === 'darwin') {
-    HOST = 'docker.for.mac.host.internal'; // Mac
-} else {
-    HOST = 'host.docker.internal'; // Linux (after patching hosts file, see chrome/docker-entrypoint.sh)
-}
+// if (process.platform === 'win32') {
+//     HOST = 'docker.for.win.host.internal'; // Windows
+// } else if (process.platform === 'darwin') {
+//     HOST = 'docker.for.mac.host.internal'; // Mac
+// } else {
+//     HOST = 'host.docker.internal'; // Linux (after patching hosts file, see chrome/docker-entrypoint.sh)
+// }
+HOST = 'host.docker.internal'; // Linux (after patching hosts file, see chrome/docker-entrypoint.sh)
 
 const PORT = 3000;
 
